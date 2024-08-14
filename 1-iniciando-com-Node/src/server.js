6d03ad21d849cc153b9e18bc0b68a2939bd2fffc
Caio Nikolas <caio.kanu@hotmail.com>
2024-08-14 17:59:20 -0300

    Iniciando com Node

--- 1-iniciando-com-Node/src/server.js
+++ 1-iniciando-com-Node/src/server.js
@@ -3,7 +3,37 @@
 
 // -- padrão novo --
 import http from 'node:http'
+/*METODOS HTTP
 
-const server = http.createServer((req, res) => {})
+GET => Buscar um recurso do back-end
+POST => Criar um recurso do back-end
+PUT => Atualizar um recurso do back-end
+PATCH => Atualizar uma informação especifica de um recurso do back-end
+DELETE => Deletar um recurso do back-end
 
-server.listen(3333)
+Cabeçados (Requisição/resposta) => Metadados
+*/
+const users = []
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
+
+    return res.end('Criação de usuários')
+  }
+
+  return res.end('Hello World')
+})
+
+server.listen(3334)
