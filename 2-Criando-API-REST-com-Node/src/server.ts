413fd5edce448987959fb904219aab3c09c31137
Caio Nikolas <caio.kanu@hotmail.com>
2024-08-18 21:48:57 -0300

    Banco de dados

--- 2-Criando-API-REST-com-Node/src/server.ts
+++ 2-Criando-API-REST-com-Node/src/server.ts
@@ -1,15 +1,31 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
+// import { randomUUID } from 'node:crypto'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server running!')
   })
+
+/*
+    .insert({
+      id: randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+  */
