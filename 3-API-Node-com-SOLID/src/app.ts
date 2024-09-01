4c1298770fac8e84cd75c6af6f51eb2f1e28fe63
Caio Nikolas <caio.kanu@hotmail.com>
2024-09-01 12:37:47 -0300

    casos de uso e design patterns

--- 3-API-Node-com-SOLID/src/app.ts
+++ 3-API-Node-com-SOLID/src/app.ts
@@ -1,6 +1,24 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.log(error)
+  } else {
+    // TODO: aqui nós deveriamos fazer o log para uma ferramenta externa como Datadog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
